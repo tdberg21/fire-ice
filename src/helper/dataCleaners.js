fa984b6cebd37cdd89aebabdc05e81fc9e73826a
Tory Dannenberg <rfdberg@gmail.com>
2018-07-26 09:40:40 -0600

    Add conditional in cleaner for houses with no founded information

--- src/helper/dataCleaners.js
+++ src/helper/dataCleaners.js
@@ -1,12 +1,16 @@
 export const scrubHouseData = data => {
   let houses = data.map(house => {
+    if (house.founded === '') {
+      house.founded = 'n/a';
+    }
     return {
       name: house.name,
       founded: house.founded,
       seats: house.seats,
       titles: house.titles,
       ancestralWeapons: house.ancestralWeapons,
-      coatOfArms: house.coatOfArms
+      coatOfArms: house.coatOfArms,
+      words: house.words
     };
   });
   return houses;
